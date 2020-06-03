3685724f36791c798c96e32163d7b92b4acb8542
Assia <48075675+01Eva@users.noreply.github.com>
2020-06-03 20:05:13 +0200

    Add files via upload

---
+++ materielrequiremenetplanning.py
@@ -0,0 +1,42 @@
+ 
+def mpr(x,ss,s0, lt):
+    
+    gr= []
+    sr= []
+    for i in range(x):
+        gr.append(int(input("insert :")))
+    for i in range(x):
+        sr.append(int(input("insert scheduled receipts:")))
+    
+    oh = [0] * x
+    nr = [0] * x
+    por = [0] * x
+
+    if s0 + ss + sr[0] > gr[0] :
+            nr[0]=0
+            oh[0]=(s0+sr[0])-gr[0]
+    else :
+         nr[0]=gr[0]- ( (s0-ss)+sr[0])
+         oh[0]=ss
+
+    i=1
+    while i < x :
+        if oh[(i-1)] + ss + sr[i] > gr[i] :
+            nr[i]=0
+            oh[i]=(oh[i-1]+sr[i])-gr[i]
+        else : 
+            nr[i]=gr[i]-((oh[i-1]-ss)+sr[i])
+            oh[i]=ss
+            por[i-lt]=nr[i]
+            sr[i]=por[i-lt]
+            
+        
+
+        i+=1
+    return [gr,sr,oh,nr,por]
+
+
+print(mpr(8,0,100,3))
+
+
+ 
